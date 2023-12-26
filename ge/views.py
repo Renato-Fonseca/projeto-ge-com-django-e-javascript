187356895f43ff30503e2188fab52bce21182b6d
Renato Fonseca <68356834+Renato-Fonseca@users.noreply.github.com>
2023-12-26 20:00:40 -0300

    .

---
+++ ge/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def table(request): 
+    return render(request, 'ge/table.html')
+
+
+def teste(request):
+    return 'devo excluir esta view function'
